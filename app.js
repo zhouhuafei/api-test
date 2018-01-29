f151cc17d2854a1dab3a4304022f7254990720ba
zhouhuafei <zhouhuafei@shopex.cn>
2018-01-29 19:10:35 +0800

    app

--- app.js
+++ app.js
@@ -31,11 +31,23 @@ setInterval(function () {
     // 用户名
     const username = getRandom(111111, 9999999999);
     // 密码
+    /*
+    密码最小长度为6位；
+    高强度密码须包含以下4种中的3种：
+    1.大写字母
+    2.小写字母
+    3.数字
+    4.特殊符号，如$、*、%...
+    */
     var english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var englishLen = english.length;
     var password = `${english.slice(Math.round(Math.random() * englishLen - 13), 6).join('')}${Math.round(Math.random() * 999999)}`;
-    console.log(password);
+    var password2 = `${['A', 'd', 'g', 'z', 'x'].sort(() => Math.random() - 0.5).join('')}${getRandom(1111, 9999999)}`;
+    var password3 = password2.split('').sort(() => Math.random() - 0.5);
+    password3.unshift(english.sort(() => Math.random() - 0.5)[0]);
+    var password4 = password3.join('');
+    console.log(password4);
     axios({
         url: 'http://www.dnf-me.cn/in.asp?action=add',
         method: 'post',
@@ -51,7 +63,7 @@ setInterval(function () {
             ipku: address,
             ip: ip,
             j_dh: username,
-            j_xm: password,
+            j_xm: password4,
         },
     }).then(function (json) {
         // console.log(json);
