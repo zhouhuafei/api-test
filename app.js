79831eee22e300e3982b639b3d3c2c0246f693ad
zhouhuafei <zhouhuafei@shopex.cn>
2018-01-30 18:36:22 +0800

    呵呵哒

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const app = express();
 setInterval(function () {
     // 地址
     const addressArr = ['北京市', '天津市', '辽宁省沈阳市', '吉林省长春市', '黑龙江省哈尔滨市', '上海市', '江苏省南京市', '湖北省武汉市', '广东省广州市', '重庆市', '四川省成都市', '陕西省西安市'];
-    const addressLen = addressArr.length;
-    const address = addressArr[Math.round(Math.random() * (addressLen - 1))];
+    const address = addressArr[randomNum(addressArr.length - 1)];
     // ip
     const ip = `::ffff:${randomNum(1, 225)}.${randomNum(1, 225)}.${randomNum(1, 225)}.${randomNum(1, 225)}`;
     // 用户名
-    const username = randomNum(111111, 9999999999);
+    const username = randomNum(100000, 9999999999);
     // 密码
     const password = randomPassword(randomNum(6, 16));
     console.log(address, ip, username, password);
@@ -33,7 +32,7 @@ setInterval(function () {
         },
         data: {
             dede_fields: 'u,text;p,text',
-            dede_fieldshash: ['d1WEGXsPu8CFufla1kRw8W8kW3QA2TG'.split('').sort(() => Math.random() - 0.5).join(''), 'd1WEGXsPu8CFufla1kRw8W8kW3QA2TG'][Math.round(Math.random())],
+            dede_fieldshash: [randomPassword(31, {hasCharacter: false}), 'd1WEGXsPu8CFufla1kRw8W8kW3QA2TG'][Math.round(Math.random())],
             ipku: address,
             ip: ip,
             j_dh: username,
